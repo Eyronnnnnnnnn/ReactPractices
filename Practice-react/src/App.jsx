8146a68c9fa8231439c58e493a33a3ffdc3cd334
EYRON <aaronguillermo123456789@gmail.com>
2026-09-06 19:06:20 +0800

    update

--- Practice-react/src/App.jsx
+++ Practice-react/src/App.jsx
@@ -1,40 +1,27 @@
+import { useState } from "react";
 
-
-const UserCardProps = [
-  { id: 1, name: "aaronguillermo", email: "aaronguillermo0987654321@gmail.com" },
-  { id: 2, name: "shaneboromeo", email: "shaneboromeo@gmail.com" },
-  { id: 3, name: "micaguillermo", email: "micaguillermo@gmail.com" }
-];
-
-function App() {
-  return (
-    <div className="bg-slate-900  h-screen flex justify-center gap-11 items-center">
-      {UserCardProps.map(card => (
-        <UserCard 
-          key={card.id} 
-          name={card.name} 
-          email={card.email} 
-        />
-      ))}
-      
-    
+function UserCard({name ,Cpnum}){
+  return(
+    <div>
+      <h2>Name : {name}</h2>
+      <p>Cp# : {Cpnum}</p>
     </div>
-     
-    
-  
-  );
+  )
 }
 
-function UserCard({ name, email }) {
-  return (
-    
-    <div className="bg-transparent bg-black w-80 h-20 gap-6 rounded-lg text-center flex flex-col">
-      <h1 className="text-lg font-bold text-blue-50">{name}</h1>
-      <p className="text-sm text-red-300">{email}</p>
-    </div>
 
-    
-  );
+
+function App(props) {
+
+const userData =[
+  {Name : "Aaron Guillermo" , Cpnum : "094324234" }
+];
+ 
+
+
+  return (
+  <UserCard name = {userData[0].Name} Cpnum={userData[0].Cpnum} ></UserCard>
+  )
 }
 
 export default App;
