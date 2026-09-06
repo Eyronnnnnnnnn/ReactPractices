de3078c173c6b1322a0019f9732475cbbd01fc87
EYRON <aaronguillermo123456789@gmail.com>
2026-09-06 17:53:26 +0800

    updated

--- Practice-react/src/App.jsx
+++ Practice-react/src/App.jsx
@@ -1,46 +1,40 @@
-import App2 from "./practice";
 
 
 const UserCardProps = [
-  { id : 1,
-  name: "aaronguillermo",
-  email: "aaronguillermo0987654321@gmail.com"
-},
-  { id : 2,
-  name: "shaneboromeo",
-  email: "shaneboromeo@gmail.com"
-},
-  { id : 3,
-  name: "micaguillermo",
-  email: "micaguillermo@gmail.com"
-},
+  { id: 1, name: "aaronguillermo", email: "aaronguillermo0987654321@gmail.com" },
+  { id: 2, name: "shaneboromeo", email: "shaneboromeo@gmail.com" },
+  { id: 3, name: "micaguillermo", email: "micaguillermo@gmail.com" }
+];
 
-]
-
-
-function App(){
-  return(
-   <div>
-     {UserCardProps.map(card =>{
-      return <UserCard key = {card.id} name = {card.name} email = {card.email}/>
-     })}
-        
-        <App2 />
-   </div>
+function App() {
+  return (
+    <div className="bg-slate-900  h-screen flex justify-center gap-11 items-center">
+      {UserCardProps.map(card => (
+        <UserCard 
+          key={card.id} 
+          name={card.name} 
+          email={card.email} 
+        />
+      ))}
+      
+    
+    </div>
+     
+    
   
   );
-
-
- 
 }
-  function UserCard(props){
-    return(
-      <div>
-        <h1>{props.name}</h1>
-        <p>{props.email}</p>
-      </div>
-    )
-  }
 
+function UserCard({ name, email }) {
+  return (
+    
+    <div className="bg-transparent bg-black w-80 h-20 gap-6 rounded-lg text-center flex flex-col">
+      <h1 className="text-lg font-bold text-blue-50">{name}</h1>
+      <p className="text-sm text-red-300">{email}</p>
+    </div>
 
-export default App
+    
+  );
+}
+
+export default App;
--- Practice-react/src/practice.jsx
+++
@@ -1,5 +0,0 @@
-export default function App2(){
-    return(
-        <h1>hello again!!</h1>
-    )
-}
