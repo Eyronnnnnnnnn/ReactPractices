e9e26993bd2a2da5b900d395d53ecc8856fba50f
EYRON <aaronguillermo123456789@gmail.com>
2026-09-07 21:00:11 +0800

    updated

--- Practice-react/src/App.jsx
+++ Practice-react/src/App.jsx
@@ -1,26 +1,17 @@
 import { useState } from "react";
 
-function UserCard({name ,Cpnum}){
-  return(
-    <div>
-      <h2>Name : {name}</h2>
-      <p>Cp# : {Cpnum}</p>
-    </div>
-  )
-}
+
 
 
 
 function App(props) {
 
-const userData =[
-  {Name : "Aaron Guillermo" , Cpnum : "094324234" }
-];
- 
-
+const taskdata = [
+{"aaronguillermo"}
+]
 
   return (
-  <UserCard name = {userData[0].Name} Cpnum={userData[0].Cpnum} ></UserCard>
+  
   )
 }
 
