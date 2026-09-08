33ca461769e251ea9484f4d07d2e3b0d75731aab
EYRON <aaronguillermo123456789@gmail.com>
2026-09-08 22:46:43 +0800

    updated

--- Practice-react/src/App.jsx
+++ Practice-react/src/App.jsx
@@ -1,10 +1,29 @@
-import { useState } from "react";
+  import { useState } from "react";
 
 
-function App() {
-  return (
- 
-  )
-}
+  function Todolist(props){
 
-export default App;
+    return(
+      <div className="w-screen h-screen bg-gray-800 flex justify-center align items-center">
+        <div >
+          <h1 className="text-white">welcome to todolist</h1>
+          <p>{props.todos.map(todo => <li className="text-white" key={todo.id}>{todo.text}</li>)}</p>
+        </div>
+      </div>
+    )
+
+  }
+
+  function App(props) {
+    const [todos, setTodos] = useState([
+      { id: 1, text: "code every day", done: false },
+      { id: 2, text: "bebe time every day", done: false },
+      { id: 3, text: "Eat Every day", done: false },
+    ]);
+
+    return (
+      <Todolist todos = {todos}/>
+    );
+  }
+
+  export default App;
