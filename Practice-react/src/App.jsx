85c795cd0b88f7c9a2700fd1b3782f5c0af393df
EYRON <aaronguillermo123456789@gmail.com>
2026-09-08 22:00:03 +0800

    updated

--- Practice-react/src/App.jsx
+++ Practice-react/src/App.jsx
@@ -1,26 +1,9 @@
 import { useState } from "react";
 
-// this is child component
-function Update_ui({count}){
-return(
-  <div>
-    <h1>Buttonclick : {count} </h1>
-  </div>
-)
-}
-
-
 
 function App() {
-
-  const [count , countFunction] = useState(0);
-
-
   return (
-  <div>
-    <button onClick={()=> countFunction(count + 1)}>click me</button>
-    <Update_ui count = {count} />
-  </div>
+ 
   )
 }
 
