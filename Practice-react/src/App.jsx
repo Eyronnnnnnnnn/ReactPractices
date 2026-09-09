0f0a9517afc5ec47e3756c7e3031bef821764052
EYRON <aaronguillermo123456789@gmail.com>
2026-09-09 21:47:20 +0800

    updated

--- Practice-react/src/App.jsx
+++ Practice-react/src/App.jsx
@@ -1,33 +1,42 @@
-  import { useState } from "react";
+import { useState } from "react";
 
-
-  function Todolist(props){
-
-    return(
-      <div className="w-screen h-screen bg-gray-800 flex justify-center align items-center">
-        <div >
-          <h1 className="text-white">welcome to todolist</h1>
-          <div>
-            {props.todos.map(todo=>(
-              <p className="text-white" key={todo.id}>{todo.text}</p>
-            ))}
-          </div>
+function Todolist(props) {
+  return (
+    <div className="w-screen h-screen bg-gray-800 flex justify-center align items-center">
+      <div>
+        <h1 className="text-white">welcome to todolist</h1>
+        <input
+          className=""
+          onChange={(event) => props.setnewTodo(event.target.value)}
+          value={props.newtodo}
+          type="text"
+          placeholder="TODO"
+        ></input>
+        <button className="w-40 h-6 bg-gray-900 text-white ">Submit</button>
+        <div>
+          {props.todos.map((todo) => (
+            <p className="text-white" key={todo.id}>
+              {todo.text}
+            </p>
+          ))}
         </div>
       </div>
-    )
+    </div>
+  );
+}
 
-  }
+function App(props) {
+  const [todos, setTodos] = useState([
+    { id: 1, text: "code every day", done: false },
+    { id: 2, text: "bebe time every day", done: false },
+    { id: 3, text: "Eat Every day", done: false },
+  ]);
 
-  function App(props) {
-    const [todos, setTodos] = useState([
-      { id: 1, text: "code every day", done: false },
-      { id: 2, text: "bebe time every day", done: false },
-      { id: 3, text: "Eat Every day", done: false },
-    ]);
+  const [newTodo, setNewTodo] = useState("");
 
-    return (
-      <Todolist todos = {todos}/>
-      /* 
+  return (
+    <Todolist todos={todos} newtodo={newTodo} setnewTodo={setNewTodo} />
+    /* 
   Dito pala ikaw magse-set ng name ng props mo.
   Kunwari yan nga `todos` ang name ng prop na naglalaman ng todos array.
   Ngayon sa child component na `Todolist`, 
@@ -35,9 +44,9 @@
   para makuha yung prop na galing sa parent.
   Kaya `props.todos.map()` ang gamit para ma-loop yung array.
 */
-    );
-  }
+  );
+}
 
-  export default App;
+export default App;
 
-  // 
+//
