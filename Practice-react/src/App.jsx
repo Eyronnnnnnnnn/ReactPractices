2b70f76a95db222ee8e05840beeb4955588c108a
EYRON <aaronguillermo123456789@gmail.com>
2026-09-10 01:31:45 +0800

    updated

--- Practice-react/src/App.jsx
+++ Practice-react/src/App.jsx
@@ -4,16 +4,18 @@ function Todolist(props) {
   return (
     <div className="w-screen h-screen bg-gray-800 flex justify-center align items-center">
       <div>
-        <h1 className="text-white">welcome to todolist</h1>
+        <h1 className="text-white font-bold text-center">WELCOME TO TODOLIST</h1>
         <input
-          className=""
+          className=" w-80"
           onChange={(event) => props.setnewTodo(event.target.value)}
           value={props.newtodo}
           type="text"
           placeholder="TODO"
         ></input>
+        <br></br>
         <button onClick={props.clickable} className="w-40 h-6 bg-gray-900 text-white  ">Submit</button>
-        <div>
+        <button className= "bg-red-900 w-40 h-6">RESET</button>
+        <div className=" h-44 overflow-auto border p-1 border-blue-900 rounded-lg ">
           {props.todos.map((todo) => (
             <p className="text-white" key={todo.id}>
               {todo.text}
@@ -47,8 +49,19 @@ function App(props) {
     done : false 
   };
 
+
   setTodos([...todos , newtodoobject ]);
+  setNewTodo("");
 }
+
+
+  function helloworld(){
+    const helloObject ={
+      id: todos.length + 1 ,
+      text : "Hello world!!",
+      done: false 
+    };
+  }
 
   return (
     <Todolist todos={todos} newtodo={newTodo} setnewTodo={setNewTodo} clickable = {handleAddTodo}/>
