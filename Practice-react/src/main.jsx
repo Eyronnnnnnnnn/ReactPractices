64f28df8b7914a13e40a67e4ad150586c6aec5b2
EYRON <aaronguillermo123456789@gmail.com>
2026-09-11 20:31:35 +0800

    updated

--- Practice-react/src/main.jsx
+++ Practice-react/src/main.jsx
@@ -2,11 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 // import App from './App.jsx'
-import practice from './practice.jsx'
+import Practice from './practice.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App  /> */}
-    <practice />
+    <Practice />
   </StrictMode>
 )
+
+
+
--- Practice-react/src/practice.jsx
+++ Practice-react/src/practice.jsx
@@ -1,12 +1,31 @@
 import { useState } from "react";
 
-export default function practice(){
 
 
-    
+function DisplayNumbers(props){
     return(
-        <div>
-            <h1>hello world</h1>
+        <div className="" >
+            <h1 className="text-black">Please Pick to display numbers</h1>
+          <button className="bg-slate-400 w-20 h-10 mr-6 ml-9 " >1</button>
+          <button className="bg-slate-400 w-20 h-10 ">2</button>
+        
+         <h1>YOU PICK : {props.number2}</h1>
+
         </div>
+    )
+}
+
+
+export default function Practice(props){
+ 
+     const [num1 , Setnum1] = useState(1);
+    const [num2 , Setnum2] = useState(2);
+
+    return(
+      
+       <DisplayNumbers 
+       number1 = {num1}
+       number2 = {num2}
+       />
     )
 }
