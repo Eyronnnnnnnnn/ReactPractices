652e79eb0ff71a799fe1ad053a3092c441d36778
EYRON <aaronguillermo123456789@gmail.com>
2026-09-11 21:01:02 +0800

    updated

--- Practice-react/src/practice.jsx
+++ Practice-react/src/practice.jsx
@@ -3,29 +3,36 @@ import { useState } from "react";
 
 
 function DisplayNumbers(props){
+
+  
+
     return(
         <div className="" >
             <h1 className="text-black">Please Pick to display numbers</h1>
-          <button className="bg-slate-400 w-20 h-10 mr-6 ml-9 " >1</button>
-          <button className="bg-slate-400 w-20 h-10 ">2</button>
+          <button className="bg-slate-400 w-20 h-10 mr-6 ml-9  "
+          onClick={()=>props.setNum(1)}
+          >1</button>
+
+          <button className="bg-slate-400 w-20 h-10 "
+          onClick={()=> props.setNum(2)}
+          >2</button>
         
-         <h1>YOU PICK : {props.number2}</h1>
+         <h1>YOU PICK : {props.shownum}</h1>
 
         </div>
     )
 }
 
-
 export default function Practice(props){
  
-     const [num1 , Setnum1] = useState(1);
-    const [num2 , Setnum2] = useState(2);
+   const [numbers , setNumbers] = useState(0);
+
 
     return(
       
        <DisplayNumbers 
-       number1 = {num1}
-       number2 = {num2}
+       shownum = {numbers}
+       setNum = {setNumbers}
        />
     )
 }
