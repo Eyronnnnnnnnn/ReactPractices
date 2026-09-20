752f021f0f149dbc908c5a0391228048f1697763
EYRON <aaronguillermo123456789@gmail.com>
2026-09-20 19:00:53 +0800

    updated

--- Practice-react/src/liftingState.jsx
+++ Practice-react/src/liftingState.jsx
@@ -1,21 +1,29 @@
-
-import {useState} from "react"
+import { useState } from "react";
 
 // child component
-function AddButton() {
-  return <div></div>;
+function AddButton(props) {
+  return (
+    <div>
+        <button
+        
+        onClick={()=>props.setCount((newCount)=> newCount + 1 )}
+        >Click for + 1</button>
+    </div>
+  )
 }
 
-
 //child component
-function Displaycount() {
-  return <div></div>;
+function Displaycount(props) {
+  return (
+    <div>
+      <h1>Number Clicked : {props.count}</h1>
+    </div>
+  );
 }
 
 //parent component
 export default function LiftingState() {
-  const [count, setCount] = useState(null);
-
+  const [count, setCount] = useState(0);    
   return (
     <div>
       <Displaycount count={count} />
